c7a7df5bd9047e14d7fe6d6d197b98f682038779
Ashish Gupta <ashishgu@andrew.cmu.edu>
2018-12-13 00:18:04 -0500

    Add gnuplot CMDs for plotting cycles

---
+++ graph_10proc_cycles.cmd
@@ -0,0 +1,8 @@
+set title 'Program Execution Time with different graphs (10 unit Processing)'
+set grid
+set key left top
+set ylabel 'Execution Time'
+set xlabel 'Number of graph nodes'
+
+plot 'results/10proc/cycles.result.graphetch' with linespoints title 'Graphetch', \
+'results/10proc/cycles.result.vanilla' with linespoints title 'Vanilla'
