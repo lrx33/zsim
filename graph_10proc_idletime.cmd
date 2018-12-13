b9a0f0d54be7667debf0d58231e3acd024f11bf4
Ashish Gupta <ashishgu@andrew.cmu.edu>
2018-12-12 23:46:00 -0500

    Add memory idle time gnuplot graph cmd files

---
+++ graph_10proc_idletime.cmd
@@ -0,0 +1,12 @@
+set title '10 unit Processing on each node'
+set grid
+set key left top
+set xlabel 'Total Program Time'
+set ylabel 'Total Memory Idle Time'
+
+plot 'results/10proc/vanilla_graphetch_1000000.cfg.10proc.out.result.reduced' with lines title '1MilNodesVanilla', \
+'results/10proc/graphetch_graphetch_1000000.cfg_10proc.out.result.reduced' with lines title '1MilNodesGraphetch', \
+'results/10proc/vanilla_graphetch_600000.cfg.10proc.out.result.reduced' with lines title '600kNodesVanilla', \
+'results/10proc/graphetch_graphetch_600000.cfg_10proc.out.result.reduced' with lines title '600kNodesGraphetch',\
+'results/10proc/vanilla_graphetch_400000.cfg.10proc.out.result.reduced' with lines title '400kNodesVanilla', \
+'results/10proc/graphetch_graphetch_400000.cfg_10proc.out.result.reduced' with lines title '400kNodesGraphetch'
---
+++ graph_500proc_idletime.cmd
@@ -0,0 +1,12 @@
+set title '500 unit Processing on each node'
+set grid
+set key left top
+set xlabel 'Total Program Time'
+set ylabel 'Total Memory Idle Time'
+
+plot 'results/500proc/vanilla_graphetch_1000000.cfg.500proc.out.result.reduced' with lines title '1MilNodesVanilla', \
+'results/500proc/graphetch_graphetch_1000000.cfg_500proc.out.result.reduced' with lines title '1MilNodesGraphetch', \
+'results/500proc/vanilla_graphetch_600000.cfg.500proc.out.result.reduced' with lines title '600kNodesVanilla', \
+'results/500proc/graphetch_graphetch_600000.cfg_500proc.out.result.reduced' with lines title '600kNodesGraphetch',\
+'results/500proc/vanilla_graphetch_400000.cfg.500proc.out.result.reduced' with lines title '400kNodesVanilla', \
+'results/500proc/graphetch_graphetch_400000.cfg_500proc.out.result.reduced' with lines title '400kNodesGraphetch'
